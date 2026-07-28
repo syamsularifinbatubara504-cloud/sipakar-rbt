a0fb0242643074d5dce723203c2603e0de11fd73
fiekryadiperdana14-star <fiekryadiperdana14-star@users.noreply.github.com>
2026-07-29 06:43:35 +0700

    fix: add fileReplacements to angular.json and update apiUrl to /api

--- rbt-frontend/src/environments/environment.ts
+++ rbt-frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: '/api',
   googleClientId: '673992786221-eom0c8k7samudu3rtmuenlcdnh2gcank.apps.googleusercontent.com',
 };
